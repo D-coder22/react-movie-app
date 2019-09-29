6895f2395676cc493ad95dbe1795e8edca449f53
D-coder22 <amitnmf@gmail.com>
2019-09-29 17:20:54 +0530

    Movie app successfully created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import MovieCard from './MovieCard.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.airtable.com/v0/appebN5ZriniJ22Cy/favorirtes?api_key=key7UGQWikZTAtvzd')
+    .then((resp) => resp.json())
+    .then(data => {
+      console.log(data);
+      this.setState({ movies: data.records });
+    }).catch(err => {
+      // Error
+    });
+  }
+  
+  render() {
+    return (
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col">
+            <div className="card-deck">
+            {this.state.movies.map(movie => <MovieCard {...movie.fields} /> )}            
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
